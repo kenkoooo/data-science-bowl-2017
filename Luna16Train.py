9ced1f365c660425b870d4e09e8ab1cce6147d56
kenkoooo <kenkou.n@gmail.com>
2017-05-08 05:51:39 +0900

    ディープラーニング道、はじめます！

--- Luna16Train.py
+++ Luna16Train.py
@@ -3,7 +3,10 @@ import random
 
 import numpy as np
 import pandas as pd
+from keras.callbacks import ModelCheckpoint
 from tqdm import tqdm
+
+import UNet
 
 random.seed(71)
 
@@ -12,14 +15,7 @@ def sample(img_array: np.ndarray, sample_list: list) -> np.ndarray:
     return np.array([img_array[i] for i in range(len(img_array)) if i in sample_list])
 
 
-def main(args):
-    annotations = pd.read_csv(args.a)
-    directory = args.d
-    output = args.o
-    patient_ids = list(set(annotations["seriesuid"]))
-    size = 128
-    k = 150
-
+def load_images(patient_ids: list, directory: str, *, size=128, k=150) -> (np.ndarray, np.ndarray):
     lungs = []
     masks = []
     for patient_id in tqdm(patient_ids):
@@ -34,9 +30,26 @@ def main(args):
         masks.append(sample(mask_array, sample_list))
     lungs = np.array(lungs)
     masks = np.array(masks)
+    return lungs, masks
+
+
+def main(args):
+    annotations = pd.read_csv(args.a)
+    directory = args.d
+    output = args.o
+    patient_ids = list(set(annotations["seriesuid"]))
+    size = 128
+
+    lungs, masks = load_images(patient_ids, directory, size=128)
+
     print(lungs.shape)
     print(masks.shape)
     print(output)
+
+    model = UNet.get_unet(size, size)
+    model_checkpoint = ModelCheckpoint('{}/weights.h5'.format(output), monitor='val_loss', save_best_only=True)
+    model.fit(lungs, masks, batch_size=32, nb_epoch=20, verbose=1, shuffle=True, validation_split=0.2,
+              callbacks=[model_checkpoint])
 
 
 if __name__ == '__main__':
