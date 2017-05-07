2e37785a31f5e18c62aea23cb515e088941001ce
kenkoooo <kenkou.n@gmail.com>
2017-05-07 19:17:50 +0900

    cropper

--- Luna16RandomCropping.py
+++ Luna16RandomCropping.py
@@ -2,6 +2,8 @@ import random
 
 import numpy as np
 
+random.seed(71)
+
 
 def load_numpy_array(patient_id: str, directory: str) -> (np.ndarray, np.ndarray):
     lung_img = np.load("{}/{}_lung_img.npz".format(directory, patient_id))
@@ -9,23 +11,39 @@ def load_numpy_array(patient_id: str, directory: str) -> (np.ndarray, np.ndarray
     return lung_img, nodule_mask
 
 
-def crop_x(lung_img: np.ndarray, nodule_mask: np.ndarray, size: int):
-    return crop(lung_img, nodule_mask, size)
+def transpose_y(img: np.ndarray) -> np.ndarray:
+    return img.transpose((1, 0, 2))
 
 
-def crop_y(lung_img: np.ndarray, nodule_mask: np.ndarray, size: int):
-    t = (1, 0, 2)
-    return crop(lung_img.transpose(t), nodule_mask.transpose(t), size)
+def transpose_z(img: np.ndarray) -> np.ndarray:
+    return img.transpose((2, 1, 0))
 
 
-def crop_z(lung_img: np.ndarray, nodule_mask: np.ndarray, size: int):
-    t = (2, 1, 0)
-    return crop(lung_img.transpose(t), nodule_mask.transpose(t), size)
+def crop(img: np.ndarray, *, idx: int, h_edge: int, w_edge: int, size: int) -> np.ndarray:
+    return img[idx, h_edge:h_edge + size, w_edge:w_edge + size]
 
 
-def crop(lung_img: np.ndarray, nodule_mask: np.ndarray, size: int):
-    layers, height, width = lung_img.shape
+def crop_tuple(img: np.ndarray, size: int) -> (int, int, int):
+    layers, height, width = img.shape
     l = random.randint(0, layers - 1)
     h = random.randint(0, height - size - 1)
     w = random.randint(0, width - size - 1)
-    return lung_img[l, h:h + size, w:w + size], nodule_mask[l, h:h + size, w:w + size]
+    return l, h, w
+
+
+def cropped_list(img: np.ndarray, mask: np.ndarray, *, size: int, num: int, axis="x") -> (np.ndarray, np.ndarray):
+    if axis == "y":
+        img = transpose_y(img)
+        mask = transpose_y(mask)
+    elif axis == "z":
+        img = transpose_z(img)
+        mask = transpose_z(mask)
+
+    img_list = []
+    mask_list = []
+    for i in range(num):
+        idx, h, w = crop_tuple(img, size)
+        img_list.append(crop(img, idx=idx, h_edge=h, w_edge=w, size=size))
+        mask_list.append(crop(mask, idx=idx, h_edge=h, w_edge=w, size=size))
+
+    return np.array(img_list), np.array(mask_list)
