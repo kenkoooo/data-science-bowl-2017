119d1bc1f4ff958e5857e6c4fd644b868b961cce
kenkoooo <kenkou.n@gmail.com>
2017-05-07 17:29:11 +0900

    multiprocessing

--- Luna16Preprocess.py
+++ Luna16Preprocess.py
@@ -1,10 +1,38 @@
 import argparse
+import multiprocessing as mp
+import time
 import traceback
 
+import pandas
 import pandas as pd
 from tqdm import tqdm
 
 from Luna16PreprocessUtils import images_and_nodules
+
+
+def load_and_dump(patient_id: str, directory: str, annotations: pandas.DataFrame) -> bool:
+    # noinspection PyBroadException
+    try:
+        images_nodules = images_and_nodules(patient_id, annotations, directory)
+        if not images_nodules:
+            return False
+
+        lung_img, nodule_mask = images_nodules
+        lung_img.dump("{}/{}_lung_img.npz".format(directory, patient_id))
+        nodule_mask.dump("{}/{}_nodule_mask.npz".format(directory, patient_id))
+        return True
+    except Exception:
+        traceback.print_exc()
+        print(patient_id)
+        return False
+
+
+def single_task(args) -> str:
+    patient_id = args[0]
+
+    if not load_and_dump(patient_id, args[1], args[2]):
+        time.sleep(0.1)
+    return patient_id
 
 
 def main(args) -> None:
@@ -13,23 +41,18 @@ def main(args) -> None:
     :param args: 
     :return: 
     """
+    pool = mp.Pool(4)
+
     annotations = pd.read_csv(args.a)
     directory = args.d
 
     patient_ids = list(set(annotations["seriesuid"]))
-    for patient_id in tqdm(patient_ids):
-        # noinspection PyBroadException
-        try:
-            images_nodules = images_and_nodules(patient_id, annotations, directory)
-            if not images_nodules:
-                continue
+    args_list = [(patient_id, directory, annotations) for patient_id in patient_ids]
+    progress_bar = tqdm(total=len(args_list))
 
-            lung_img, nodule_mask = images_nodules
-            lung_img.dump("{}/{}_lung_img.npz".format(directory, patient_id))
-            nodule_mask.dump("{}/{}_nodule_mask.npz".format(directory, patient_id))
-        except Exception:
-            traceback.print_exc()
-            print(patient_id)
+    for _ in pool.imap_unordered(single_task, args_list):
+        progress_bar.update(1)
+    progress_bar.close()
 
 
 if __name__ == '__main__':
